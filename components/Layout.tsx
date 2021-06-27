39892b2fe27bdcd39e68a0c1b6841a38b0300e86
hideo54 <hideo54@hideo54.com>
2021-06-27 22:16:43 +0900

    Create Layout and top

---
+++ components/Layout.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import Head from 'next/head';
+
+const Layout = ({
+    children,
+    title = 'hideo54 Lab',
+    description = '新しいプロダクトにするほどでもないような、小さな制作物や研究結果を公開する場所。',
+    imageUrl = 'https://img.hideo54.com/icons/main.png',
+    twitterCardType = 'summary',
+}: {
+    children?: ReactNode;
+    title?: string;
+    description?: string;
+    imageUrl?: string;
+    twitterCardType?: 'summary' | 'summary_large_image' | 'app' | 'player';
+    Header?: JSX.Element;
+}) => (
+    <>
+        <Head>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            <meta name='description' content={description} />
+            <meta key='og:site_name' property='og:site_name' content='hideo54 Lab' />
+            <meta key='og:title' property='og:title' content={title} />
+            <meta key='og:description' property='og:description' content={description} />
+            {imageUrl &&
+                <meta property='og:image' content={imageUrl} />
+            }
+            <meta key='twitter:card' name='twitter:card' content={twitterCardType} />
+            <meta key='twitter:site' name='twitter:site' content='@hideo54' />
+            <title>{title}</title>
+            <link rel='icon' type='image/png' href='https://img.hideo54.com/icons/main.png' />
+            <link rel='apple-touch-icon' href='https://img.hideo54.com/icons/main.png' />
+        </Head>
+        <main>
+            {children}
+        </main>
+    </>
+);
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import Layout from '../components/Layout';
+
 const App = () => {
-    return <p>Hello</p>;
+    return (
+        <Layout>
+            <div style={{ textAlign: 'center' }}>
+                <h1>
+                    Welcome to
+                    <div>hideo54 Lab</div>
+                </h1>
+                <p>小さな制作物や研究結果を公開する場所です。</p>
+            </div>
+        </Layout>
+    );
 };
 
 export default App;
