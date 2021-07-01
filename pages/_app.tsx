bc7d7bb40f6845ba369bd6c60f627701a83e0d2c
hideo54 <hideo54@hideo54.com>
2021-07-01 18:22:57 +0900

    Create top page

---
+++ pages/_app.tsx
@@ -0,0 +1,49 @@
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        -webkit-text-size-adjust: 100%;
+    }
+
+    body, select, button {
+        font-family: -apple-system, BlinkMacSystemFont, 'Hiragino Sans', 'Noto Sans JP', sans-serif;
+        margin: 0;
+    }
+
+    header, main, footer {
+        display: block; /* for IE */
+    }
+
+    main {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 0 1em;
+    }
+
+    footer {
+        margin-top: 3em;
+        margin-bottom: 1em;
+        padding: 0 1em;
+    }
+
+    h1, h2, h3, h4, h5, h6, p, div {
+        color: #333333;
+    }
+
+    span {
+        color: #333333;
+        line-height: 100%;
+    }
+`;
+
+const App = ({ Component, pageProps }: AppProps) => {
+    return (
+        <>
+            <Component {...pageProps} />
+            <GlobalStyle />
+        </>
+    );
+};
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
+import styled from 'styled-components';
+import { Flask } from '@styled-icons/ionicons-solid';
 import Layout from '../components/Layout';
+
+const HeadDiv = styled.div`
+    text-align: center;
+`;
+
+const CatchH1 = styled.h1`
+    font-family: 'Noto Sans JP', sans-serif;
+    font-weight: 100;
+    div {
+        font-size: 2em;
+        line-height: 1.2;
+    }
+`;
 
 const App = () => {
     return (
         <Layout>
-            <div style={{ textAlign: 'center' }}>
-                <h1>
+            <HeadDiv>
+                <div style={{
+                    padding: '1em',
+                }}>
+                    <Flask size='200px' color='silver' />
+                </div>
+                <CatchH1>
                     Welcome to
                     <div>hideo54 Lab</div>
-                </h1>
-                <p>小さな制作物や研究結果を公開する場所です。</p>
-            </div>
+                </CatchH1>
+                <p>小さな制作物や研究結果を公開しています。</p>
+            </HeadDiv>
         </Layout>
     );
 };
