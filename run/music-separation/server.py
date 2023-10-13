21b280ef13de2afe9b691430186bd5db9bfa9ff5
Hideo Yasumoto <hideo54@hideo54.com>
2023-10-13 18:42:40 +0900

    Create a complete API for music separation

--- run/music-separation/server.py
+++ run/music-separation/server.py
@@ -2,25 +2,54 @@ import os
 
 from flask import Flask, request
 from flask_cors import CORS
+from yt_dlp import YoutubeDL
+import demucs.separate
+from google.cloud import storage
+
+bucket_url = 'https://img.hideo54.com'
+bucket_name = 'img.hideo54.com'
+model = 'htdemucs'
+stems = ['vocals', 'bass', 'drums', 'other']
 
 app = Flask(__name__)
 CORS(app)
 
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucket_name)
+
 @app.route('/', methods=['POST'])
 def main():
     url = request.json.get('url')
-    print(url)
 
-    # TODO: Exec yt-dlp
+    yt_dlp_options = {
+        'format': 'bestaudio',
+        'outtmpl': 'tmp/original/%(extractor_key)s_%(id)s.mp3',
+    }
+    with YoutubeDL(yt_dlp_options) as ydl:
+        yt_dlp_result = ydl.extract_info(url, download=True)
+        original_mp3_filename = yt_dlp_result['requested_downloads'][0]['filename']
 
-    # TODO: Exec spleeter
+    demucs.separate.main([
+        '-o', 'tmp',
+        '-n', model,
+        '--filename', '{track}_{stem}.{ext}',
+        '--jobs', 0,
+        '--mp3',
+        original_mp3_filename,
+    ])
 
-    # TODO: Upload to Cloud Storage
+    result_urls = dict()
 
-    # TODO: Return URL
+    for stem in stems:
+        source_filename = f'tmp/{model}/{stem}'
+        dest_filename = 'music-separation/' + original_mp3_filename.replace('.mp3', f'_{model}_stem.mp3')
+        blob = bucket.blob(dest_filename)
+        upload_result = blob.upload_from_filename(source_filename)
+        result_urls[stem] = f'{bucket_url}/music-separation/{dest_filename}'
 
     return {
         'ok': True,
+        'result': result_urls,
     }
 
 if __name__ == '__main__':
